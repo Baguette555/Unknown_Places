ba065842e27576f9f865af31f3e30178e82bb455
Baguette555 <Maxoulol124@gmail.com>
2024-04-25 20:17:47 +0200

    Boss Distance Script
    What the script does :
    - Track the distance between the player and the boss.
    - If you're close, the player's icon will enter phase "losing" whilst the boss' icon will enter phase "winning"
    - At a correct distance, both will be in "neutral"
    - Being far away from the boss will show "Winning" icon for the Player, and "Losing" icon for the boss.
    * The values can change at anytime *
    
    + Added player icons (need to be reworked)

--- Assets/BossDistance.cs
+++ Assets/BossDistance.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,6 +14,7 @@ public class BossDistance : MonoBehaviour
     public Transform player;
 
     [SerializeField] Image characterIcon;
+    [SerializeField] Image bossIcon;
 
     [SerializeField] Sprite danger;
     [SerializeField] Sprite neutral;
@@ -29,13 +31,48 @@ public class BossDistance : MonoBehaviour
         distanceText.text = "DISTANCE : ";
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float dist = Vector3.Distance(boss.position, player.position);      // Baser sur x : ça marche pas des masses
-        Debug.Log(dist);
-        int distInt = (int)dist;
-        //int distInt = Convert.ToInt32(dist);
-        Debug.Log(distInt);
-        distanceText.text = "DISTANCE : " + (distInt - 17) + " m";
+        float playerX = player.position.x;
+        float bossX = boss.position.x;
+
+        float distX = Mathf.Abs(playerX - bossX);
+
+        int distInt = Mathf.RoundToInt(distX);
+        distanceText.text = "DISTANCE : " + (distInt - 6) + " m";
+
+        if(distInt <= 12)
+        {
+            characterIcon.sprite = danger;
+            bossIcon.sprite = bossWinning;
+        }
+        else if(distInt >= 13 && distInt <= 20) 
+        {
+            characterIcon.sprite = neutral;
+            bossIcon.sprite = bossNeutral;
+        }
+        else
+        {
+            characterIcon.sprite = winning;
+            bossIcon.sprite = bossLosing;
+        }
+
+
     }
 }
+
+/*   void Start()
+   {
+       distanceText.text = "DISTANCE : ";
+   }
+
+   void FixedUpdate()
+   {
+       float dist = Vector3.Distance(boss.position, player.position);      // Baser sur x : ça marche pas des masses
+       Debug.Log(dist);
+       int distInt = (int)dist;
+       //int distInt = Convert.ToInt32(dist);
+       Debug.Log(distInt);
+       distanceText.text = "DISTANCE : " + (distInt - 17) + " m";
+   }
+}*/
