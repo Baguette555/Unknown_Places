c5940a6a9da73c54076993911e58c634194416f7
Baguette555 <117479545+Baguette555@users.noreply.github.com>
2024-04-30 11:08:43 +0200

    Boss Distance Script updated
    + Placeholder for Boss Icons
    + Large Floor added as placeholder
    = Removed old BossDistance Script

--- Assets/BossDistance.cs
+++ Assets/BossDistance.cs
@@ -60,19 +60,3 @@ public class BossDistance : MonoBehaviour
 
     }
 }
-
-/*   void Start()
-   {
-       distanceText.text = "DISTANCE : ";
-   }
-
-   void FixedUpdate()
-   {
-       float dist = Vector3.Distance(boss.position, player.position);      // Baser sur x : ça marche pas des masses
-       Debug.Log(dist);
-       int distInt = (int)dist;
-       //int distInt = Convert.ToInt32(dist);
-       Debug.Log(distInt);
-       distanceText.text = "DISTANCE : " + (distInt - 17) + " m";
-   }
-}*/
