39355deaf161cff55aa43278c4cfc1c5b077ffb5
Baguette555 <Maxoulol124@gmail.com>
2024-05-12 14:30:00 +0200

    Cutscene + Hold2Skip script
    Intro Cutscene done (except the art stuff)
    Hold To Skip script done too

--- Assets/Hold2Skip.cs
+++ Assets/Hold2Skip.cs
@@ -1,20 +1,25 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Hold2Skip : MonoBehaviour
 {
-    [SerializeField] Button button;
-    [SerializeField] Image progressCircle;
+    [SerializeField] Animator textAnimator;
+    [SerializeField] Image holdToSkipImage;
+    [SerializeField] Image progressCircleImage;
     [SerializeField] float holdTime = 2f; // Temps en secondes pour atteindre 100%
+    [SerializeField] float fillSpeed = 1f;
+    [SerializeField] float alphaSpeed = 1f;
     [SerializeField] bool isHolding = false;
     [SerializeField] float holdStartTime;
+    private float circleFillState = 1f;
 
     public LevelLoader LevelLoader;
 
-    void Start()
+    private void Start()
     {
-        button.onClick.AddListener(OnButtonClicked);
+        textAnimator.Play("HoldToSkip");
     }
 
     void Update()
@@ -23,30 +28,49 @@ public class Hold2Skip : MonoBehaviour
         {
             float holdDuration = Time.time - holdStartTime;
             float progress = Mathf.Clamp01(holdDuration / holdTime); // Progression entre 0 et 100%
+            progressCircleImage.fillAmount = progress;
 
-            progressCircle.fillAmount = progress;
+            SetAlphaAndFill(progress);
 
             if (holdDuration >= holdTime)
             {
                 LevelLoader.LoadNextLevel();
             }
         }
+        else
+        {
+            circleFillState = Mathf.Clamp01(circleFillState - fillSpeed * Time.deltaTime);
+            progressCircleImage.fillAmount = circleFillState;
+
+            float newAlpha = Mathf.Clamp01(holdToSkipImage.color.a - alphaSpeed * Time.deltaTime);
+            SetAlphaAndFill(newAlpha);
+
+            if (holdToSkipImage.color.a == 0)
+            {
+                textAnimator.enabled = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            textAnimator.enabled = false;
+            isHolding = true;
+            holdStartTime = Time.time;
+        }
+        else if (Input.GetKeyUp(KeyCode.F))
+        {
+            isHolding = false;
+        }
     }
 
-    void OnButtonClicked()
+    void SetAlphaAndFill(float alpha)   // Modifie l'alpha et le remplissage du cercle et du texte
     {
-        Debug.Log("Button clicked!");
-    }
+        Color textColor = holdToSkipImage.color;
+        textColor.a = alpha;
+        holdToSkipImage.color = textColor;
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isHolding = true;
-        holdStartTime = Time.time;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        isHolding = false;
-        progressCircle.fillAmount = 0f; // Réinitialise la progression du cercle
+        float newFillAmount = Mathf.Clamp01(progressCircleImage.fillAmount + fillSpeed * Time.deltaTime);
+        progressCircleImage.fillAmount = newFillAmount;
+        progressCircleImage.color = new Color(progressCircleImage.color.r, progressCircleImage.color.g, progressCircleImage.color.b, alpha);
     }
 }
